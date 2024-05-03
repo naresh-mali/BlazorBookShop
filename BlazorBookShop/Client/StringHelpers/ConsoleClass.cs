399095df4b272a4e930ba7f34d1e485fd997790e
Naresh Mali <nareshmali302@gmail.com>
2024-05-03 20:38:59 +0530

    Added Class and Learn basic fundamentals

---
+++ BlazorBookShop/Client/StringHelpers/ConsoleClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBookShop.Client.StringHelpers
+{
+    public class ConsoleClass
+    {
+        public static void Method1()
+        {
+            Console.WriteLine("Hello Blazor");
+        }
+    }
+}
